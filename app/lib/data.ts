59e5dea95f2acb1858eb1f00651b7310d119aa41
Tatsiana Sh <tatsiana15az@gmail.com>
2024-03-07 09:00:04 -0700

    Fetch data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,6 +11,7 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
+  // noStore();
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
@@ -19,11 +20,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
